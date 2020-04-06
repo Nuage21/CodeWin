bf15a86a36963c0190a74f51a48e4741c1a37db4
hbFree <ih_beldjoudi@esi.dz>
2020-04-06 23:15:52 +0100

    smth added

--- src/Question.java
+++ src/Question.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Question{
     int ID; // unique ID for avery question
     String core; // Question declarative text ex: What sign does it represent ?
+    int points; // question reward
     ArrayList<String> propositions; // since all questions are QCMs ~ contains all propositions
     ArrayList<Boolean> correct_answers; // for each proposition ^^ a boolean indicating if correct or false
 }
