5ff69542983c33e996353c8342609d8c9e3d8af2
namgalf <58704494+namgalf@users.noreply.github.com>
2020-05-02 11:10:37 +0200

    deco

--- src/AccountPane_Controller.java
+++ src/AccountPane_Controller.java
@@ -1,8 +1,13 @@
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AccountPane_Controller {
 
@@ -34,9 +39,28 @@ public class AccountPane_Controller {
         });
 
         daddyVBox.setOnMouseExited( event -> {
-            if (isMouseEntered)
+            if (isMouseEntered);
                 stage.close();
         });
+
+        signoutPane.setOnMouseClicked(e->{
+            if(isMouseEntered){
+
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("Authenticator.fxml"));
+                    Parent root = null ;
+                    root = loader.load();
+                    Stage stg = Settings.appStage;
+                    Scene sc = new Scene(root);
+                    LoggedIn_Controller.setUser(null);
+                    stg.setScene(sc);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+
     }
 
     public void setStage(Stage stage) {
